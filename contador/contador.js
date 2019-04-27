72e4eb0200924b0d98f9abc25be606b0314a20b6
Anselmo <valenzuela.anselmo71@gmail.com>
2019-04-27 12:55:27 -0300

    8 agregarmos contador

---
+++ contador/contador.js
@@ -0,0 +1,7 @@
+var cantidadDeVideos
+cantidadDeVideos =  0
+
+function agregar () {
+    cantidadDeVideos = cantidadDeVideos +  1 
+    document.getElementById('contador-videos').innerHTML = cantidadDeVideos
+}
