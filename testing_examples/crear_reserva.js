67b50e1ebb46a10a260763801254ba6cdd3328f5
alejandro-panza <alejandrofpanza@gmail.com>
2019-10-04 19:00:06 -0300

    crear reserva

---
+++ testing_examples/crear_reserva.js
@@ -0,0 +1,6 @@
+const confirmar = () => { alert('se ha reservado correctamente')}
+const habilitarDesdeHasta = () => {
+    document.getElementById('desde').disabled = false
+    document.getElementById('hasta').disabled = false
+}
+const habilitarHabitaciones = () => document.getElementById('habitaciones').style.display = 'block'
